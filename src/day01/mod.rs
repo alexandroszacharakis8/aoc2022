3857f3e6f5c5c99a9e229ba966e5d132d990727d
Alexandros Zacharakis <alexandros.zacharakis8@gmail.com>
2022-12-01 07:52:45 +0100

    add day1

---
+++ src/day01/mod.rs
@@ -0,0 +1,33 @@
+use std::str::FromStr;
+
+use itertools::Itertools;
+
+pub fn part1() {
+    let input = include_str!("input");
+    let result = input
+        .split("\n\n")
+        .map(|el| 
+            el
+            .lines()
+            .map(|line| u64::from_str(line).unwrap()).collect_vec())
+            .max_by(|x,y| x.iter().sum::<u64>().cmp(&y.iter().sum::<u64>()))
+            .unwrap()
+            .iter()
+            .sum::<u64>();
+    println!("result: {:?}", result);
+}
+
+pub fn part2() {
+    let input = include_str!("input");
+    let result = input
+        .split("\n\n")
+        .map(|el| 
+            el
+            .lines()
+            .map(|line| u64::from_str(line).unwrap()).collect_vec())
+            .sorted_by(|x,y| y.iter().sum::<u64>().cmp(&x.iter().sum::<u64>()))
+            .take(3)
+            .flatten()
+            .sum::<u64>();
+    println!("result: {:?}", result);
+}
